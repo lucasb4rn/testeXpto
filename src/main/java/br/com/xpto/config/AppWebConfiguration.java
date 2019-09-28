5aeeeee8a566adbd25376528f1afd77ba20e43c7
Lucasb4rngit config --global user.email Lucas.b4rn@gmail.comgit config --global user.name Lucasb4rn <Lucas.b4rn@gmail.com>
2019-09-28 02:58:59 -0300

    Adicionado pom imports padrões para utilizar spring, e as classes de configurações padrões

---
+++ src/main/java/br/com/xpto/config/AppWebConfiguration.java
@@ -0,0 +1,39 @@
+package br.com.xpto.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages ={""})  
+public class AppWebConfiguration extends WebMvcConfigurerAdapter {
+	
+	@Bean
+	public InternalResourceViewResolver internalResourceViewResolver(){
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+	
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		// 
+		configurer.enable();
+	}
+	
+	@Bean
+	public MultipartResolver multipartResolver(){
+		return new StandardServletMultipartResolver();
+	}
+	
+	
+}
