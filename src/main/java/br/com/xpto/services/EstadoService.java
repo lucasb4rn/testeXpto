5bfdbe235d57985bbccd4902957b6f3197b7035e
Lucasb4rngit config --global user.email Lucas.b4rn@gmail.comgit config --global user.name Lucasb4rn <Lucas.b4rn@gmail.com>
2019-09-28 03:17:06 -0300

    Adicionado os services para utilização dos repositorys

---
+++ src/main/java/br/com/xpto/services/EstadoService.java
@@ -0,0 +1,10 @@
+package br.com.xpto.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EstadoService {
+	
+
+
+}
---
+++ src/main/java/br/com/xpto/services/RegiaoService.java
@@ -0,0 +1,9 @@
+package br.com.xpto.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegiaoService {
+
+	
+}
